8ce1006705bc7ea0fbfe21c7575b6f64c5107de7
Irwin Estrada Torres <irwinestradatorres@gmail.com>
2025-05-23 17:02:40 -0500

    Creando el Template de Páginas en WordPress

---
+++ wp-content/themes/escueladecocina/page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+    <?php while(have_posts()): the_post(); ?>
+        <!-- <?php the_post_thumbnail(); ?> -->
+        <?php 
+            $html = edc_imagen_destacada(get_the_ID()); 
+            // $html[0] retorna el HTML
+            // $html[1] retorna si la imagen existe
+            echo $html[0];
+        ?>    
+
+        <main class="container">
+            <div class="row justify-content-center">
+                <div class="py-3 px-5 contenido-pagina bg-light <?php echo $html[1] ? 'col-md-8 destacada' : 'col-md-12 nodestacada'; ?>">
+                    <h1 class="text-center my-5 separador"><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </main>
+    <?php endwhile; ?>
+
+<?php get_footer(); ?>
